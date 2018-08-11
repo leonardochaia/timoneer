5f5f77f834c1f31a67e32ac3d307ed60f44ada8a
Leonardo Chaia <lchaia@astonishinglab.com>
2018-08-10 21:07:15 -0300

    chore(hmr): destroy all cdk overlays when reloading

--- src/hmr.ts
+++ src/hmr.ts
@@ -6,6 +6,13 @@ export const hmrBootstrap = (module: any, bootstrap: () => Promise<NgModuleRef<a
     module.hot.accept();
     bootstrap().then(mod => ngModule = mod);
     module.hot.dispose(() => {
+
+        // HACK: destroy all cdk overlays since they break with HMR.
+        const overlays = document.getElementsByClassName('cdk-overlay-container');
+        for (let i = 0; i < overlays.length; i++) {
+            overlays[i].innerHTML = '';
+        }
+
         const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
         const elements = appRef.components.map(c => c.location.nativeElement);
         const makeVisible = createNewHosts(elements);
