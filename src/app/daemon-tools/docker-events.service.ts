63f9dfd53535be490085f7c5002cfcd95cd5a701
Leonardo Chaia <lchaia@astonishinglab.com>
2018-08-12 00:19:03 -0300

    fix: list not updating after os sleep

--- src/app/daemon-tools/docker-events.service.ts
+++ src/app/daemon-tools/docker-events.service.ts
@@ -4,6 +4,7 @@ import { DockerEvent } from 'dockerode';
 import { switchMap, takeUntil, startWith } from 'rxjs/operators';
 import { DockerService } from './docker.service';
 import { streamToObservable } from './stream-to-observable';
+import { ElectronService } from '../electron-tools/electron.service';
 
 @Injectable()
 export class DockerEventsService implements OnDestroy {
@@ -12,25 +13,29 @@ export class DockerEventsService implements OnDestroy {
 
   private disposed = new Subject();
 
-  constructor(dockerService: DockerService,
-    ngZone: NgZone) {
+  private unbinded = new Subject();
 
-    dockerService.docker(d => d.getEvents())
-      .pipe(
-        switchMap(stream => streamToObservable<Buffer>(stream, ngZone)),
-        takeUntil(this.disposed)
-      )
-      .subscribe(chunk => {
-        const decoded = JSON.parse(chunk.toString('utf8')) as DockerEvent;
-        if (this.events.has(decoded.Action)) {
-          this.events.get(decoded.Action).next(decoded);
-        }
+  constructor(private dockerService: DockerService,
+    private ngZone: NgZone,
+    electron: ElectronService) {
 
-        const combined = `${decoded.Type}.${decoded.Action}`;
-        if (this.events.has(combined)) {
-          this.events.get(combined).next(decoded);
+    const monitor = electron.remote.powerMonitor;
+
+    // Bind and unbind when the machine suspends/resumes.
+    monitor.on('suspend', () => {
+      this.unbindFromDocker();
+    });
+
+    monitor.on('resume', () => {
+      setTimeout(() => {
+        if (!this.unbinded.closed) {
+          this.unbindFromDocker();
         }
-      });
+        this.bindToDocker();
+      }, 2000);
+    });
+
+    this.bindToDocker();
   }
 
   public ngOnDestroy() {
@@ -50,5 +55,31 @@ export class DockerEventsService implements OnDestroy {
 
   public bindAll(events: string[], type?: string) {
     return combineLatest(events.map(e => this.bind(type ? `${type}.${e}` : e).pipe(startWith(null))));
+  }
+
+  private unbindFromDocker() {
+    this.unbinded.next();
+    this.unbinded.unsubscribe();
+  }
+
+  private bindToDocker() {
+    this.unbinded = new Subject<any>();
+    this.dockerService.docker(d => d.getEvents())
+      .pipe(
+        switchMap(stream => streamToObservable<Buffer>(stream, this.ngZone)),
+        takeUntil(this.disposed),
+        takeUntil(this.unbinded),
+    )
+      .subscribe(chunk => {
+        const decoded = JSON.parse(chunk.toString('utf8')) as DockerEvent;
+        if (this.events.has(decoded.Action)) {
+          this.events.get(decoded.Action).next(decoded);
+        }
+
+        const combined = `${decoded.Type}.${decoded.Action}`;
+        if (this.events.has(combined)) {
+          this.events.get(combined).next(decoded);
+        }
+      });
   }
 }
