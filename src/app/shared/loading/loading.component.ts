981713958f2151be038f7eba7ee7bac0435a8b6f
Leonardo Chaia <lchaia@astonishinglab.com>
2018-09-28 13:08:37 -0300

    refactor: all loadings are now a progress bar

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,12 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tim-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent {
-
-  @Input()
-  public alternative: boolean;
-}
+export class LoadingComponent { }
