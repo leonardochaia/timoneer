60c79a72ba3a225fff44b86fa51617716b0f7713
Leonardo Chaia <lchaia@astonishinglab.com>
2018-10-24 21:15:20 -0300

    feat: added link to create a container in container list

--- src/app/daemon-tools/container-list-container/container-list-container.component.ts
+++ src/app/daemon-tools/container-list-container/container-list-container.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TimoneerTabs } from '../../timoneer-tabs';
+import { TabService } from '../../tabs/tab.service';
 
 @Component({
   selector: 'tim-container-list-container',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class ContainerListContainerComponent {
 
+  constructor(private readonly tab: TabService) { }
+
+  public createContainer() {
+    this.tab.add(TimoneerTabs.DOCKER_CONTAINER_NEW);
+  }
 }
